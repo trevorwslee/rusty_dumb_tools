44b52890f4d637a4508f8b96b7a4a7f5b120e66e
Trevor Lee <31684028+trevorwslee@users.noreply.github.com>
2024-02-13 21:28:37 +0800

    Update styles for calculator buttons

--- demos/web_based_calculator/src/main.rs
+++ demos/web_based_calculator/src/main.rs
@@ -26,7 +26,7 @@ fn App() -> impl IntoView {
     };
     view! {
         <table>
-            <tr><td class="display_td" colspan=6>{
+            <tr><td class="display_td" colspan=6> {
                 move || {
                     let mut calculator = calculator_ref.borrow_mut();
                     let pressed_chars = pressed_key.get();
@@ -40,7 +40,7 @@ fn App() -> impl IntoView {
                         {display.chars().map(|c| {
                                 let c = if c == ' ' { "".to_string() } else { c.to_string() };
                                 view! {
-                                    <span class="display_digit_span">
+                                    <span class="display_digit_button">
                                         { c }
                                     </span>
                                 }
@@ -50,42 +50,50 @@ fn App() -> impl IntoView {
             }
             </td></tr>
             <tr>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="square">x<span style="vertical-align: super; font-size: 50%">2</span></button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="sqrt"><span style="vertical-align: super; font-size: 50%">2</span>{"√"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="inv">{"1/x"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="abc">{"|x|"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="(">{"("}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=")">{")"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="sin">{"sin"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="cos">{"cos"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="tan">{"tan"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="asin">{"sin"}<span class="ss_span">-1</span></button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="acos">{"cos"}<span class="ss_span">-1</span></button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="atan">{"tan"}<span class="ss_span">-1</span></button></td>
             </tr>
             <tr>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="pow10">10<span style="vertical-align: super; font-size: 50%">x</span></button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=7>{"7️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=8>{"8️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=9>{"9️⃣"}</button></td>
-                <td class="key_td" colspan=2><button class="digit_span" on:click=on_key_pressed value="ac">{"AC"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="square">x<span class="ss_span">2</span></button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="sqrt">{"√"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="inv">{"1/x"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="abc">{"|x|"}</button></td>
+                <td class="key_td" style="background-color:lightyellow"><button class="digit_button" on:click=on_key_pressed value="(">{"("}</button></td>
+                <td class="key_td" style="background-color:lightyellow"><button class="digit_button" on:click=on_key_pressed value=")">{")"}</button></td>
             </tr>
             <tr>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="log">{"log"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=4>{"4️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=5>{"5️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=6>{"6️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="*">{"✖️"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="/">{"➗"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="pow10">10<span class="ss_span">x</span></button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=7>{"7️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=8>{"8️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=9>{"9️⃣"}</button></td>
+                <td class="key_td" style="background-color:orange" colspan=2><button class="digit_button" on:click=on_key_pressed value="ac">{"AC"}</button></td>
             </tr>
             <tr>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="ln">{"ln"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=1>{"1️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=2>{"2️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=3>{"3️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="+">{"➕"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="-">{"➖"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="log">{"log"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=4>{"4️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=5>{"5️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=6>{"6️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="*">{"✖️"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="/">{"➗"}</button></td>
             </tr>
             <tr>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="%">{"%"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value="neg">{"±"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=0>{"0️⃣"}</button></td>
-                <td class="key_td"><button class="digit_span" on:click=on_key_pressed value=".">{"•"}</button></td>
-                <td class="key_td" colspan=2><button class="digit_span" on:click=on_key_pressed value="=">{"🟰"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="ln">{"ln"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=1>{"1️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=2>{"2️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=3>{"3️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="+">{"➕"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="-">{"➖"}</button></td>
+            </tr>
+            <tr>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="%">{"%"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value="neg">{"±"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=0>{"0️⃣"}</button></td>
+                <td class="key_td"><button class="digit_button" on:click=on_key_pressed value=".">{"•"}</button></td>
+                <td class="key_td" style="background-color:lightgreen" colspan=2><button class="digit_button" on:click=on_key_pressed value="=">{"🟰"}</button></td>
             </tr>
         </table>
     }
